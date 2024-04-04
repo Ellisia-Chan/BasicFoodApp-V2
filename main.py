514fe32950460e9f3d893aa62f366510648d4c53
Ren <152279955+ChristianJude23@users.noreply.github.com>
2024-04-04 20:02:59 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
-from tkinter import filedialog, messagebox
+from tkinter import messagebox
 from tkinter.font import BOLD
+import os
 
 #Color Palette: https://colorhunt.co/palette/b3c8cfbed7dcf1eedce5ddc5
 
@@ -34,19 +35,39 @@ class Login(tk.Tk):
         lbl_usesrname.place(x=20, y=80)
         lbl_password.place(x=20, y=120)
 
-        ent_username = tk.Entry(frame, width=18, font=("Arial", 16), bd=3, relief=tk.GROOVE)
-        ent_password = tk.Entry(frame, width=18, font=("Arial", 16), bd=3, relief=tk.GROOVE)
+        self.ent_username = tk.Entry(frame, width=18, font=("Arial", 16), bd=3, relief=tk.GROOVE)
+        self.ent_password = tk.Entry(frame, width=18, font=("Arial", 16), bd=3, relief=tk.GROOVE, show="*")
 
-        ent_username.place(x=130, y=80)
-        ent_password.place(x=130, y=120)
+        self.ent_username.place(x=130, y=80)
+        self.ent_password.place(x=130, y=120)
 
-        btn_enter = tk.Button(frame, text="Enter", font=("Arial", 16), bg="#BED7DC", width=10)
+        btn_enter = tk.Button(frame, text="Enter", font=("Arial", 16), bg="#BED7DC", width=10, command=self.enter)
         btn_enter.place(x=120, y=190)
+
+    def enter(self):
+        username = self.ent_username.get()
+        password = self.ent_password.get()
+
+        script_dir = os.path.dirname(os.path.realpath(__file__))
+        file_path = os.path.join(script_dir, "data.txt")
+
+        with open(file_path, "r") as file:
+            for line in file:
+                stored_username, stored_password = line.strip().split(",")
+                if username == stored_username and password == stored_password:
+                    if username == "admin":
+                        self.destroy()
+                        win = Window()
+                        win.mainloop()
+                        break
+                    
+
 
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title("")
+        self.title("Basic Food App")
+
 
 app = Login()
 app.mainloop()
